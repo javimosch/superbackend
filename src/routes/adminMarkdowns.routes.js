09117a1b5c4c7605969191a8be749379c04f41ab
Jarancibia <arancibiajav@gmail.com>
2026-03-04 20:47:42 +0100

    feat: Add list, group codes, and folder content endpoints for admin markdowns

--- src/routes/adminMarkdowns.routes.js
+++ src/routes/adminMarkdowns.routes.js
@@ -5,6 +5,9 @@ const { adminSessionAuth } = require('../middleware/auth');
 const adminMarkdownsController = require('../controllers/adminMarkdowns.controller');
 
 router.use(adminSessionAuth);
+router.get('/', adminMarkdownsController.list);
+router.get('/group-codes/:category', adminMarkdownsController.getGroupCodes);
+router.get('/folder/:category/:group_code', adminMarkdownsController.getFolderContents);
 router.post('/validate-path', adminSessionAuth, adminMarkdownsController.validatePath);
 
 module.exports = router;
