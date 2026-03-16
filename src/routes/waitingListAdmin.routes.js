6149a2f2fefd278300d0640cb1834557feb5f10a
Jarancibia <arancibiajav@gmail.com>
2026-03-16 23:39:21 +0100

    feat: Add CRUD routes for public exports management

--- src/routes/waitingListAdmin.routes.js
+++ src/routes/waitingListAdmin.routes.js
@@ -9,4 +9,10 @@ router.get('/types', adminSessionAuth, asyncHandler(waitingListController.getTyp
 router.get('/export-csv', adminSessionAuth, asyncHandler(waitingListController.exportCsv));
 router.post('/bulk-remove', adminSessionAuth, asyncHandler(waitingListController.bulkRemove));
 
+// Public exports management
+router.get('/public-exports', adminSessionAuth, asyncHandler(waitingListController.getPublicExports));
+router.post('/public-exports', adminSessionAuth, asyncHandler(waitingListController.createPublicExport));
+router.put('/public-exports/:id', adminSessionAuth, asyncHandler(waitingListController.updatePublicExport));
+router.delete('/public-exports/:id', adminSessionAuth, asyncHandler(waitingListController.deletePublicExport));
+
 module.exports = router;
